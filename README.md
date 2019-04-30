c00952038c628d0a29db9acffa20e25dc1f46f1e
Jonathan Blair <jon@tothemun.com>
2019-04-29 19:32:17 -0700

    Creating development boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# wsj-ar-demo
+# MUN 8th Wall Web AR Boilerplate
+
+This repo allows us to quickly and easily spin up an 8th Wall Three.js scene
+
+![tapplace-threejs-screenshot](../../../images/screenshot-tap.jpg)
+
+
+## Stack
+
+* Webpack 
+* Babel
+* Three.js
+* MUN Three Utils
+
+
+
+## Todo:
+
+* Add typescript support
+* Move 8th wall to modules
+* Add normalize.css
+* Add css modules
---
+++ config/webpack.config.dev.js
@@ -0,0 +1,120 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const getClientEnvironment = require('./env');
+const paths = require('./paths');
+
+
+// Webpack uses `publicPath` to determine where the app is being served from.
+// In development, we always serve from the root. This makes config easier.
+const publicPath = '/';
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+const publicUrl = '';
+// Get environment variables to inject into our app.
+const env = getClientEnvironment(publicUrl);
+
+// style files regexes
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+module.exports = {
+  mode: 'development',
+    // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
+  // See the discussion in https://github.com/facebook/create-react-app/issues/343
+  devtool: 'cheap-module-source-map',
+  resolve: {
+    alias: {
+      _assets: path.resolve(__dirname, '../src/assets'),
+      _scripts: path.resolve(__dirname, '../src/scripts'),
+      _styles: path.resolve(__dirname, '../src/styles'),
+      _index: path.resolve(__dirname, '../public/index.html')
+    }
+  },
+  entry: [
+    // Include an alternative client for WebpackDevServer. A client's job is to
+    // connect to WebpackDevServer by a socket and get notified about changes.
+    // When you save a file, the client will either apply hot updates (in case
+    // of CSS changes), or refresh the page (in case of JS changes). When you
+    // make a syntax error, this client will display a syntax error overlay.
+    // Note: instead of the default WebpackDevServer client, we use a custom one
+    // to bring better experience for Create React App users. You can replace
+    // the line below with these two lines if you prefer the stock client:
+    // require.resolve('webpack-dev-server/client') + '?/',
+    // require.resolve('webpack/hot/dev-server'),
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    // Finally, this is your app's code:
+    paths.appIndexJs,
+    // We include the app code last so that if there is a runtime error during
+    // initialization, it doesn't blow up the WebpackDevServer client, and
+    // changing JS code would still trigger a refresh.
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './',
+    filename: './scripts/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '../',
+              emitFile: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '../images',
+              emitFile: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/main.css'
+    })
+  ]
+};
